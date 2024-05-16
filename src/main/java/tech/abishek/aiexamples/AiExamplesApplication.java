691ee7766290b7aabd8ecdafbf9d6567e7482353
Abishek ☀️ <ageddall@asu.edu>
2024-05-16 12:00:38 -0700

    Initial commit

---
+++ src/main/java/tech/abishek/aiexamples/AiExamplesApplication.java
@@ -0,0 +1,13 @@
+package tech.abishek.aiexamples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiExamplesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiExamplesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tech/abishek/aiexamples/AiExamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.abishek.aiexamples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiExamplesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
